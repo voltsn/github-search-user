e519c99dc4f226612b54899e4afd6394eb94eb1b
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-05-28 18:25:08 +0200

    Extend text and background color properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.tsx'],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        theme: {
+          accent: "var(--color-text-accent)",
+          "accent-2": "var(--color-text-accent-2)",
+          base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+          neutral: "var(--color-text-neutral)",
+        },
+      },
+      backgroundColor: {
+        theme: {
+          base: "var(--color-bg-base)",
+          "button-base": "(var(--color-button-base)",
+          muted: "var(--color-bg-muted)",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
